94ef80913be465bb65baac4af9ac8d534cff4d1a
tecnoImperions <juanrevollo001@gmail.com>
2026-02-18 10:20:35 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,57 +1,57 @@
 const CACHE_NAME = "varian-cache-v1";
 
 const urlsToCache = [
-"/base/",
-"/base/index.html",
-"/base/login.html",
-"/base/register.html",
-"/base/video.json",
+  "/musica/",
+  "/musica/index.html",
+  "/musica/login.html",
+  "/musica/register.html",
+  "/musica/video.json",
 
-"/base/public/index.html",
-"/base/public/play.html",
-"/base/public/favorites.html",
-"/base/public/playlists.html",
-"/base/public/profile.html",
-"/base/public/manifest.json",
+  "/musica/public/index.html",
+  "/musica/public/play.html",
+  "/musica/public/favorites.html",
+  "/musica/public/playlists.html",
+  "/musica/public/profile.html",
+  "/musica/public/manifest.json",
 
-"/base/assets/js/player.js",
-"/base/assets/js/auth.js",
-"/base/assets/js/songs.js",
-"/base/assets/js/supabase.js",
+  "/musica/assets/js/player.js",
+  "/musica/assets/js/auth.js",
+  "/musica/assets/js/songs.js",
+  "/musica/assets/js/supabase.js",
 
-"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
-"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css",
 
-"https://res.cloudinary.com/duwvw6q2c/image/upload/v1765742101/varian_lbd26r.webp"
+  "https://res.cloudinary.com/duwvw6q2c/image/upload/v1765742101/varian_lbd26r.webp"
 ];
 
 self.addEventListener("install", event => {
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => {
-console.log("Cacheando archivos...");
-return cache.addAll(urlsToCache);
-})
-);
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log("Cacheando archivos...");
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener("activate", event => {
-event.waitUntil(
-caches.keys().then(keys =>
-Promise.all(
-keys.map(key => {
-if (key !== CACHE_NAME) {
-return caches.delete(key);
-}
-})
-)
-)
-);
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
 });
 
 self.addEventListener("fetch", event => {
-event.respondWith(
-caches.match(event.request)
-.then(response => response || fetch(event.request))
-);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
